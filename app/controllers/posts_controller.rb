de8ca7ac7c2b6121263136af85efcb5fd6437014
takashi oka <takashi.oka@gmail.com>
2021-04-02 17:50:17 +0900

    controller・migrate更新

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,59 @@
 class PostsController < ApplicationController
+  before_action :set_rustle, only: [:show, :edit, :update, :destroy]
+  
   def index
-    # @post = Post.all
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(rustle_param)
+    if params[:back]
+       render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "you rustle my heart!"
+      else
+        render :new
+      end
+    end
+  end
+
+  def confirm
+    @post = Post.new(post_param)
+    render :new if @post.invalid?
+  end
+
+  def edit
+  end
+
+  def show 
+  end
+ 
+  def update
+    if @post.update(rustle_param)
+      redirect_to posts_path, notice: "You have edited your rustle!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "You have deleted your rustle!"
+  end
+
+  private
+  
+  def post_param
+    params.require(:post).permit(:content)
+  end
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
