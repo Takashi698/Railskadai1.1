0b45ea145a8437e8dbc9efcff095c1b0662dfa76
takashi oka <takashi.oka@gmail.com>
2021-04-02 17:59:28 +0900

    routes更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get '/postss', to: 'posts#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :rustles do
+    collection do
+      post :confirm
+    end
+  end
 end
